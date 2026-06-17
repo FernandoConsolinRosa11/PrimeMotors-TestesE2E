2a31a6a0c7f0a2874ef3cf8f9d24ce911ca94e43
FernandoConsolinRosa <fernandoconsolinrosa11@gmail.com>
2026-06-17 15:03:53 -0300

    fix(rotas):ajuste de rota

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -2,18 +2,18 @@ import { test, expect } from "@playwright/test";
 
 test.describe("User Login", () => {
   test("Login Realizado Com Sucesso!", async ({ page }) => {
-    await page.goto("https://localhost/Login");
+    await page.goto("/Login");
 
     await page.locator('input[type="email"]').fill("Fernando@gmail.com");
     await page.locator('input[type="password"]').fill("Senha@123");
     await page.click('button[type="submit"]');
 
     //Verificar redirecionamento
-    await page.goto("https://localhost/", { timeout: 10000 });
+    await page.goto("/", { timeout: 10000 });
   });
 
   test("Login Teste Falha! ", async ({ page }) => {
-    await page.goto("https://localhost/Login");
+    await page.goto("/Login");
 
     await page.locator('input[type="email"]').fill("Fernando@gmail");
     await page.locator('input[type="password"]').fill("Senha@123");
